fe09475e2a4495774b65c882ae015be886424557
itshizhan <itshizhan@163.com>
2018-12-11 20:20:50 +0800

    docs

---
+++ docs-site/components/ImgBox.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+const ImgBox = (props)=>(
+  <img src={props.src} style={{width:"460px"}}></img>
+)
+
+export default  ImgBox
--- doczrc.js
+++ doczrc.js
@@ -4,15 +4,31 @@ export default {
   description: 'Oovui react components docs site',
   src:'./docs-site',
   dest: '/docs-dist',
+  //base:"/"
+  //propsParser:false,
+  files: '**/*.{markdown,mdx}',
+  codeSandbox: false,
   themeConfig: {
     colors: {
       primary: 'tomato',
     },
     styles: {
       h1: {
-        fontSize: 30,
+        fontSize: 32,
+      },
+      h2: {
+        fontSize: 22,
+      },
+      h3: {
+        fontSize: 18,
+      },
+      h4: {
+        fontSize: 16,
+      },
+      h5: {
+        fontSize: 14,
       },
     },
-  },
+  }
 
 }
